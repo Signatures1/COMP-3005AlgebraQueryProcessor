d8fb3fb1b6ccda8b4abaf416e5b49fc6bbbe2f20
Sheng Zhang <shengzhang@dhcp-116-123.cu-wireless-staff.carleton.ca>
2025-09-15 17:28:02 -0400

    update 2

---
+++ operations.py
@@ -0,0 +1,25 @@
+from data_structures import Relation
+
+def select(relation, condition):
+    new_rows = []
+    for row in relation.rows:
+        if condition(row):
+            new_rows.append(row.copy())
+
+    return Relation(
+        name = relation.name + "_select",
+        attributes = relation.attributes,
+        rows = new_rows
+    )
+
+def project(relation, columns):
+    new_rows = []
+    for row in relation.rows:
+        new_row = {col: row[col] for col in columns if col in row}
+        new_rows.append(new_row)
+
+    return Relation(
+        name = relation.name + "_project",
+        attributes = columns,
+        rows = new_rows
+    )
