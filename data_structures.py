63964d1b43606f078bab04b98163b1d71f08f92a
Sheng Zhang <shengzhang@dhcp-116-123.cu-wireless-staff.carleton.ca>
2025-09-15 17:02:02 -0400

    first update

--- data_structures.py
+++ data_structures.py
@@ -1,9 +1,24 @@
+'''
+Relation Class
+This class represents my database relation
+Each relation object stores:
+- name: The name of the relation
+- attributes: A list of attribute names (columns)
+- rows: A list of tuples, where each tuple represents a row of data
+Example:
+    Employees (EID, Name, Age) = {
+        {"EID": "E1", "Name": "John", "Age": 32},
+        {"EID": "E2", "Name": "Alice", "Age": 28}
+    }
+This class also include a print method to display the relation's details
+'''
 class Relation:
-    def function __init__(self, name, attributes, rows):
-        self.name = name
-        self.attributes = attributes  
-        self.rows = rows  
-    def print_tables():
-        print(f"Atributes: {self.attributes}")
+    def __init__(self, name, attributes, rows):     #Constructor (For new relation objects)
+        self.name = name                            #Name of the relation
+        self.attributes = attributes                #Attributes (columns) of the relation
+        self.rows = rows                            #Data rows of the relation
+
+    def print_tables(self):                         #Method to print the relation's details
+        print(f"Attributes: {self.attributes}")     #Print attributes
         for row in self.rows:
-            print(f"Value: {row}")    
+            print(f"Value: {row}")                  #Print each row's values
